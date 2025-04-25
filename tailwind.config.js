021406aa3e803e314aee911dc2a017a331eb51ac
Fadhli Nur Himawan <fadhlinurhimawan@gmail.com>
2025-04-26 06:38:22 +0700

    build: first init

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1DA1F2",
+        secondary: "#14171A",
+        accent: "#657786",
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+      spacing: {
+        128: "32rem",
+        144: "36rem",
+      },
+      screens: {
+        xs: "475px",
+        "3xl": "1600px",
+      },
+      boxShadow: {
+        custom: "0 4px 30px rgba(0, 0, 0, 0.1)",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+      transitionProperty: {
+        width: "width",
+        spacing: "margin, padding",
+      },
+    },
+  },
+  plugins: [],
+};
