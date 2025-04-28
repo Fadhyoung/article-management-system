b3888bad408db79d21f0cdee6d9230f3bd2d5d2a
Fadhli Nur Himawan <fadhlinurhimawan@gmail.com>
2025-04-28 20:43:20 +0700

    fix: add domain vercel app

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,13 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['drive.google.com', 'nkihbopqxauxphmaqvey.supabase.co'],
+    domains: ['drive.google.com', 'nkihbopqxauxphmaqvey.supabase.co', 'your-vercel-app-name.vercel.app'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.vercel.app',
+      },
+    ],
   },
 };
 
